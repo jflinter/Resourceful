6d22f3873f347e5fc97255ca0aa21eca7e70b504
Jack Flintermann <jack@stripe.com>
2015-10-09 00:28:43 -0400

    use xcode script inputs/outputs

--- resourceful.go
+++ resourceful.go
@@ -57,14 +57,16 @@ func main() {
 	app.Usage = "Add strong typing to imageNamed: in Swift apps"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:  "input, i",
-			Value: "Images.xcassets",
-			Usage: "The xcassets directory that contains your app's images",
+			Name:   "input, i",
+			Value:  "Images.xcassets",
+			Usage:  "The xcassets directory that contains your app's images",
+			EnvVar: "SCRIPT_INPUT_FILE_0",
 		},
 		cli.StringFlag{
-			Name:  "output, o",
-			Value: "Resourceful.swift",
-			Usage: "The destination file for generated Swift code",
+			Name:   "output, o",
+			Value:  "Resourceful.swift",
+			Usage:  "The destination file for generated Swift code",
+			EnvVar: "SCRIPT_OUTPUT_FILE_0",
 		},
 		cli.BoolFlag{
 			Name:  "warn, w",
